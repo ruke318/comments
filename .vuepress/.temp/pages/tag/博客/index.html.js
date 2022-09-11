5355db283c0a63c40423db4bd727e99b6d0ab9e4
陈仁焕 <chenrenhuan@chenrenhuandeMacBook-Pro-2.local>
2022-09-11 22:08:41 +0800

    first

--- .vuepress/.temp/pages/tag/博客/index.html.js
+++ .vuepress/.temp/pages/tag/博客/index.html.js
@@ -1,14 +1,1 @@
-export const data = JSON.parse("{\"key\":\"v-3a134560\",\"path\":\"/tag/%E5%8D%9A%E5%AE%A2/\",\"title\":\"博客 标签\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"博客 标签\",\"blog\":{\"type\":\"category\",\"name\":\"博客\",\"key\":\"tag\"},\"layout\":\"Blog\",\"summary\":\"\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://ruke.xyz/tag/%E5%8D%9A%E5%AE%A2/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"技术宅男子\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"博客 标签\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}]]},\"excerpt\":\"\",\"headers\":[],\"readingTime\":{\"minutes\":0,\"words\":0},\"filePathRelative\":null}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
+export const data = JSON.parse("{\"key\":\"v-3a134560\",\"path\":\"/tag/%E5%8D%9A%E5%AE%A2/\",\"title\":\"博客 标签\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"博客 标签\",\"blog\":{\"type\":\"category\",\"name\":\"博客\",\"key\":\"tag\"},\"layout\":\"Blog\",\"summary\":\"\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://ruke.xyz/tag/%E5%8D%9A%E5%AE%A2/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"技术宅男子\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"博客 标签\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}]]},\"excerpt\":\"\",\"headers\":[],\"git\":{},\"readingTime\":{\"minutes\":0,\"words\":0},\"filePathRelative\":null}")
